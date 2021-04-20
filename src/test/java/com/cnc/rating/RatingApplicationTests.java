37b49a75ffb41c16c9457240ac66ac8bfe8c19fe
moonjihyeon1994 <puppy613@hanmail.net>
2021-04-20 17:01:06 +0900

    first commit

---
+++ src/test/java/com/cnc/rating/RatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnc.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
