d4fa7c9b45d90fd00a4bb3c6df7d2d03a862f902
moonjihyeon1994 <puppy613@hanmail.net>
2021-08-18 14:55:48 +0900

    test

--- src/main/java/com/cnc/rating/service/AzureQueryService.java
+++ src/main/java/com/cnc/rating/service/AzureQueryService.java
@@ -171,7 +171,7 @@ public class AzureQueryService {
     public void test() {
         long currentTimeMillis = System.currentTimeMillis();
         HashMap<String, Object> params = new HashMap<>();
-        params.put("service_mgmt_no", 7000406188L);
+        params.put("service_mgmt_no", "7000406188");
         List<HashMap<String, Object>> result = postgresMapper.test(params);
         log.info("duration : {}, size : {} ", System.currentTimeMillis() - currentTimeMillis, result.size());
     }
